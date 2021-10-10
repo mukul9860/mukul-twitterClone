d4b7e38ba786ce6fb7df5c5c53785efeaf97db44
Mukul Chhanikar <mukulchhanikar@gmail.com>
2021-10-10 09:53:22 +0530

    first commit

---
+++ tweets/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+import random
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+# Create your models here.
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)    
+
+
+class Tweet(models.Model):
+    # used foreign keys, so that many users can have many tweets or content.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)    
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "content" : self.content,
+            "likes" : random.randint(0, 150)
+        }
